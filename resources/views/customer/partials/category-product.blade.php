a3879befc1f309636494dfcb00837cc541c263d8
Ahnaf Bawedan <99602807+ahnafbwd@users.noreply.github.com>
2025-05-04 17:52:58 +0700

    [UPDATE] Add link to all product filter by category

--- resources/views/customer/partials/category-product.blade.php
+++ resources/views/customer/partials/category-product.blade.php
@@ -1,17 +1,18 @@
 <div class="container relative">
-    <div class="grid grid-cols-1 items-center">
-        <h3 class="text-2xl leading-normal font-semibold">Kategori</h3>
+    <div class="grid items-center grid-cols-1">
+        <h3 class="text-2xl font-semibold leading-normal">Kategori</h3>
     </div><!--end grid-->
 
     <div class="grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 mt-8 gap-[30px]">
         @foreach ($allCategory as $category)
             <div
-                class="group relative overflow-hidden hover:shadow-lg hover:dark:shadow-gray-800 rounded-md duration-500 p-6 text-center">
+                class="relative p-6 overflow-hidden text-center duration-500 rounded-md group hover:shadow-lg hover:dark:shadow-gray-800">
                 <img src="{{ asset('storage/' . $category->icon) }}"
-                    class="rounded-full shadow-md dark:shadow-gray-800 size-20 block mx-auto mb-2"
+                    class="block mx-auto mb-2 rounded-full shadow-md dark:shadow-gray-800 size-20"
                     alt="{{ $category->title }}">
 
-                <a href="" class="font-semibold hover:text-indigo-600 text-lg">{{ $category->title }}</a>
+                <a href="{{ route('customer.product.index', ['category' => $category->id]) }}"
+                    class="text-lg font-semibold hover:text-indigo-600">{{ $category->title }}</a>
             </div><!--end content-->
         @endforeach
     </div><!--end grid-->
